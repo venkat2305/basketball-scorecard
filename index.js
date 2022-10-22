123e9001757702f3ab9bd3e9cf71f17f61275a02
venkat sai ram <109469065+venkat2023@users.noreply.github.com>
2022-10-22 19:33:41 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+function homeadd1(){
+    document.getElementById("home-score").textContent++
+}
+
+function homeadd2(){
+    document.getElementById("home-score").textContent++
+    document.getElementById("home-score").textContent++
+}
+
+function homeadd3(){
+    document.getElementById("home-score").textContent++
+    document.getElementById("home-score").textContent++
+    document.getElementById("home-score").textContent++
+}
+
+function guestadd1(){
+    document.getElementById("guest-score").textContent++
+
+}
+
+function guestadd2(){
+    document.getElementById("guest-score").textContent++
+    document.getElementById("guest-score").textContent++
+}
+
+function guestadd3(){
+    document.getElementById("guest-score").textContent++
+    document.getElementById("guest-score").textContent++
+    document.getElementById("guest-score").textContent++
+}
+
+
+
